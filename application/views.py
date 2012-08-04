d800decb3a36519e2dd86826f660f5fa4f62cf5c
Stavros Korokithakis <stavros@korokithakis.net>
2012-08-04 22:26:05 +0300

    Send mail to admins on new leads.

--- application/views.py
+++ application/views.py
@@ -1,5 +1,8 @@
 from flask import render_template, flash, url_for, redirect
 
+from google.appengine.api import mail
+from google.appengine.api import app_identity
+
 from models import SignupModel
 from decorators import admin_required
 from forms import SignupForm
@@ -10,6 +13,11 @@ def index():
     form = SignupForm()
     if form.validate_on_submit():
         SignupModel(email=form.email.data).put()
+        mail.send_mail_to_admins(
+                sender="landing@%s.appspotmail.com" % app_identity.get_application_id(),
+                subject="New landing lead for %s." % app_identity.get_application_id(),
+                body=form.email.data,
+        )
         flash(u'Thank you for your interest!', 'success')
         return redirect(url_for('index'))
     return render_template('index.html', form=form)
